181ca2cba857730a6f549ce5cbc8918aa119f205
Goner <skrapziboom@gmail.com>
2026-02-07 14:33:11 +0300

    add pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,7 @@
-import { StopWatchItem } from "./StopWatchItem";
-import { useStopWatchList } from "./hooks/useStopWatchList.ts";
-import './App.scss'
+import StopWatch from "./pages/StopWatch";
 
 const App = () => {
-  const item = useStopWatchList();
-  
-  return (
-    <div className="app">
-      <h1 className="app__title">Stopwatch App</h1>
-      <button onClick={item.addStopWatch} className="app__add-button">Add Stopwatch</button>
-      <div className="app__list">
-        {item.stopwatches.map(sw => (
-          <StopWatchItem
-            key={sw.id}
-            stopwatch={sw}
-            onStart={item.startStopWatch}
-            onPause={item.pauseStopWatch}
-            onResume={item.resumeStopWatch}
-            onClear={item.clearStopWatch}
-            onDelete={item.deleteStopWatch}
-          />  
-        ))}
-      </div>
-    </div>
-  );
+  return <StopWatch />;
 }
-export default App
+
+export default App;
